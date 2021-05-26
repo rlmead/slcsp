a87bc2826b48d6eaf4572933ac300538ba0eb67a
Rebecca Mead <beckyleemead@gmail.com>
2021-05-25 22:28:05 -0400

    closes #2

--- findSLCSP.py
+++ findSLCSP.py
@@ -1,3 +1,10 @@
+import pandas
+
+plans = pandas.read_csv('./plans.csv')
+slcsp = pandas.read_csv('./slcsp.csv')
+zips = pandas.read_csv('./zips.csv')
+
+
 # Define function to calculate second lowest cost silver plan
 def main():
     print("zipcode,rate")

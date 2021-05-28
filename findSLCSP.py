58038d54eb32434fcd8d1ae919b03d7d05ab1a93
Rebecca Mead <beckyleemead@gmail.com>
2021-05-28 15:37:28 -0400

    get list of zips that are unambiguous by rate area

--- findSLCSP.py
+++ findSLCSP.py
@@ -8,6 +8,12 @@ zips = pandas.read_csv('./zips.csv')
 # Define function to calculate second lowest cost silver plan
 def main():
     # 1. Get list of zips with their state+rate_area, where unambiguous
+    
+    # a. consolidate unique combinations of zip+state+rate_area into just the first occurrence of each combination
+    zipsByRateArea = zips[['zipcode','state','rate_area']].drop_duplicates(['zipcode','state','rate_area'],keep='first')
+
+    # b. remove *all* occurrences of duplicate zip codes (now, duplicates indicate ambiguity by rate area)
+    unambiguousZips = zipsByRateArea.drop_duplicates(['zipcode'],keep=False)
 
     # 2. Consolidate silver plans by state+rate_area
 

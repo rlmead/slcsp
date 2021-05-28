9b845c3315af1d99ca2ee5d688689530c9fc782e
Rebecca Mead <beckyleemead@gmail.com>
2021-05-28 14:35:30 -0400

    pseudocode data manipulation plan

--- findSLCSP.py
+++ findSLCSP.py
@@ -1,12 +1,19 @@
 import pandas
 
+# Read the necessary data from csv files into dataframes
 plans = pandas.read_csv('./plans.csv')
 slcsp = pandas.read_csv('./slcsp.csv')
 zips = pandas.read_csv('./zips.csv')
 
-
 # Define function to calculate second lowest cost silver plan
 def main():
+    # 1. Get list of zips with their state+rate_area, where unambiguous
+
+    # 2. Consolidate silver plans by state+rate_area
+
+    # 3. Query across first two tables to find SLCSP per zip code
+
+    # 4. Return the necessary info 
     print("zipcode,rate")
 
 if __name__ == "__main__":

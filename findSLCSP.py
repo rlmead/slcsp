f5fb621164fe4bda848ceca34fea92f7b087a854
Rebecca Mead <beckyleemead@gmail.com>
2021-05-29 18:37:57 -0400

    remove duplicates from list of rates before choosing second highest

--- findSLCSP.py
+++ findSLCSP.py
@@ -5,10 +5,12 @@ plans = pandas.read_csv('./plans.csv')
 slcsp = pandas.read_csv('./slcsp.csv',dtype={'zipcode': 'str'})
 zips = pandas.read_csv('./zips.csv',dtype={'zipcode': 'str'})
 
-# Define helper function to get and format the second item in a list of dollar amount rates
-def getSecondItem(list):
-    if len(list) > 1:
-        return str('{:.2f}'.format(round(list[1],2)))
+# Define helper function to get and format the second-highest item in a list of dollar amount rates
+def getSecondHighest(inputList):
+    uniqueOrderedList = list(set(inputList))
+    uniqueOrderedList.sort()
+    if len(uniqueOrderedList) > 1:
+        return str('{:.2f}'.format(round(uniqueOrderedList[1],2)))
     else:
         return ''
 
@@ -22,8 +24,8 @@ def main():
     # b. Remove *all* occurrences of duplicate zip codes (these duplicates are ambiguous by rate area, so their slcsp can't be calculated)
     unambiguousZips = zipsByRateArea.drop_duplicates(['zipcode'],keep=False)
 
-    # 2. Gather silver plan rates by state+rate_area and create a new dataframe with second-lowest-cost-per-area rates, where they exist
-    slcspRates = plans[plans['metal_level'] == 'Silver'].sort_values('rate').groupby(['state','rate_area'])['rate'].apply(list).apply(getSecondItem).reset_index()
+    # 2. Gather silver plan rates by state+rate_area, order them from low to high, and create a new dataframe with second-lowest-cost-per-area rates, where they exist
+    slcspRates = plans[plans['metal_level'] == 'Silver'].groupby(['state','rate_area'])['rate'].apply(list).apply(getSecondHighest).reset_index()
 
     # 3. Merge slcsp, unambiguousZips, and slcspRates dataframes to find rate per zip code
     outputData = slcsp.drop('rate', axis='columns').merge(unambiguousZips,how='left',on='zipcode').merge(slcspRates,how='left',on=['state','rate_area']).fillna('')
